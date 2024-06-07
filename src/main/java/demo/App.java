e3b5c214eaad1e11768d4d3f484744609a8cfc0c
rahul <darpansarode@gmail.com>
2024-06-07 15:13:15 +0530

    Update the TestCase  without Screenshot

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -11,16 +11,17 @@ public class App {
         // This is to remove unnecessary warnings from your console
         System.setProperty("java.util.logging.config.file", "logging.properties");
         
-        TestCases tests = new TestCases(); // Initialize your test class
+        // TestCases tests = new TestCases(); // Initialize your test class
 
         //TODO: call your test case functions one after other here
 
-        tests.testCase01();
+        //tests.testCase01();
+        QkartSanity.main(null);
 
         //END Tests
 
 
-        tests.endTest(); // End your test by clearning connections and closing browser
+        // tests.endTest(); // End your test by clearning connections and closing browser
     }
 
     public static void main(String[] args) throws InterruptedException, MalformedURLException {
